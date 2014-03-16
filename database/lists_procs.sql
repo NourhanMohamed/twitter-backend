d06286b00355bc77fa84d7093ca12070a38e858e
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-03-16 12:07:30 +0200

    added lists subscriptions and memberships related procs

--- database/lists_procs.sql
+++ database/lists_procs.sql
@@ -4,6 +4,12 @@ RETURNS void AS $$
   BEGIN
     INSERT INTO lists(name, description, creator_id, private, created_at)
     VALUES (name, description, creator_id, private, created_at);
+
+    DECLARE list_id integer;
+    SELECT L.id INTO list_id FROM lists L
+    WHERE L.name = $1 AND L.creator_id = $3;
+
+    SELECT subscribe(list_id, creator_id, created_at);
   END; $$
 LANGUAGE PLPGSQL;
 
@@ -24,6 +30,60 @@ RETURNS void AS $$
   END; $$
 LANGUAGE PLPGSQL;
 
--- subscribe/unsubscribe
--- add/remove members
--- list members/subscribers
+CREATE OR REPLACE FUNCTION subscribe(user_id integer, list_id integer, created_at timestamp)
+RETURNS void AS $$
+  BEGIN
+    INSERT INTO subscriptions(subscriber_id, list_id, created_at)
+    VALUES (user_id, list_id, created_at)
+  END; $$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION unsubscribe(user_id integer, list_id integer)
+RETURNS void AS $$
+  BEGIN
+    DELETE FROM subscriptions S
+    WHERE S.subscriber_id = $1 AND S.list_id = $2;
+  END; $$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION add_member(user_id integer, list_id integer)
+RETURNS void AS $$
+  BEGIN
+    INSERT INTO memberships(member_id, list_id, created_at)
+    VALUES (user_id, list_id, created_at)
+  END; $$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION delete_member(user_id integer, list_id integer)
+RETURNS void AS $$
+  BEGIN
+    DELETE FROM memberships M
+    WHERE M.member_id = $1 AND S.list_id = $2;
+  END; $$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION get_list_subscribers(list_id integer)
+RETURNS refcursor AS $$
+DECLARE cursor refcursor := 'cur';
+  BEGIN
+    OPEN cursor FOR
+    SELECT U.name, U.username, U.avatar_file_name
+    FROM lists L INNER JOIN subscriptions S ON L.id = S.list_id
+      INNER JOIN users U ON U.id = S.subscriber_id
+    WHERE L.id = list_id;
+    RETRUN cursor;
+  END; $$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION get_list_members(list_id integer)
+RETURNS refcursor AS $$
+DECLARE cursor refcursor := 'cur';
+  BEGIN
+    OPEN cursor FOR
+    SELECT U.name, U.username, U.avatar_file_name
+    FROM lists L INNER JOIN memberships M ON L.id = M.list_id
+      INNER JOIN users U ON U.id = M.member_id
+    WHERE L.id = list_id;
+    RETRUN cursor;
+  END; $$
+LANGUAGE PLPGSQL;
