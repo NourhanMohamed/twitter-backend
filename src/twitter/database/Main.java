beb4a0c53b4d404d191f427bff9beebca66d9f81
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-04-11 16:07:11 +0200

    main testing class

---
+++ src/twitter/database/Main.java
@@ -0,0 +1,34 @@
+package twitter.database;
+
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+	private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+	public static void main(String[] args) {
+		PostgresConnection.initSource();
+		CommandsMap.instantiate();
+		HashMap<String, String> map = new HashMap<>();
+		map.put("app", "user");
+		map.put("method", "report_user");
+		map.put("reported_id", "29");
+		map.put("creator_id", "21");
+
+		Class<?> cmdClass = CommandsMap.queryClass(map.get("method"));
+		if (cmdClass == null) {
+			LOGGER.log(Level.SEVERE,
+					"Invalid Request. Class \"" + map.get("method")
+							+ "\" Not Found");
+		} else {
+			try {
+				Command c = (Command) cmdClass.newInstance();
+				c.setMap(map);
+				c.execute();
+			} catch (InstantiationException | IllegalAccessException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			}
+		}
+	}
+}
