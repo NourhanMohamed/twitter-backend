f76b258ad73d332805d55912ebbcfa0cc9d37735
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-04-11 16:06:35 +0200

    Changed class to a static

--- src/twitter/database/PostgresConnection.java
+++ src/twitter/database/PostgresConnection.java
@@ -17,73 +17,70 @@ import org.apache.commons.pool2.ObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 
 public class PostgresConnection {
-	Logger lgr = Logger.getLogger(PostgresConnection.class.getName());
-	String dbUsername = "nour";
-	String dbPassword = "";
-	String dbName = "twitter";
-	String dbInitialConnections = "5";
-	String dbMaxConnections = "10";
-	String connUri;
-	PoolingDriver dbDriver;
-	PoolingDataSource<PoolableConnection> dataSource;
+	private static final Logger LOGGER = Logger
+			.getLogger(PostgresConnection.class.getName());
+	private static final String DB_USERNAME = "nour";
+	private static final String DB_PASSWORD = "";
+	private static final String DB_NAME = "twitter";
+	private static final String DB_INIT_CONNECTIONS = "10";
+	private static final String DB_MAX_CONNECTIONS = "15";
+	private static final String URI = "jdbc:postgresql://localhost:5432/"
+			+ DB_NAME;
+	private static PoolingDriver dbDriver;
+	private static PoolingDataSource<PoolableConnection> dataSource;
 
-	public PostgresConnection() {
-		connUri = "jdbc:postgresql://localhost:5432/" + dbName;
-		dataSource = initSource();
-	}
-
-	public void shutdownDriver() throws SQLException {
-		dbDriver.closePool(dbName);
+	public static void shutdownDriver() throws SQLException {
 		shutdownConnection();
+		dbDriver.closePool(DB_NAME);
 	}
-	
-	private void shutdownConnection() throws SQLException {
+
+	private static void shutdownConnection() throws SQLException {
 		dataSource.getConnection().close();
 	}
 
-	public void printDriverStats() throws SQLException {
+	public static void printDriverStats() throws SQLException {
 		ObjectPool<? extends Connection> connectionPool = dbDriver
-				.getConnectionPool(dbName);
+				.getConnectionPool(DB_NAME);
 
-		System.out.println("DB Active Connections: " + connectionPool.getNumActive());
-		System.out.println("DB Idle Connections: " + connectionPool.getNumIdle());
+		System.out.println("DB Active Connections: "
+				+ connectionPool.getNumActive());
+		System.out.println("DB Idle Connections: "
+				+ connectionPool.getNumIdle());
 	}
 
-	private PoolingDataSource<PoolableConnection> initSource() {
+	public static PoolingDataSource<PoolableConnection> getDataSource() {
+		return dataSource;
+	}
+
+	public static void initSource() {
 		try {
 			try {
 				Class.forName("org.postgresql.Driver");
 			} catch (ClassNotFoundException ex) {
-				lgr.log(Level.SEVERE,
+				LOGGER.log(Level.SEVERE,
 						"Error loading Postgres driver: " + ex.getMessage(), ex);
 			}
 
 			Properties props = new Properties();
-			props.setProperty("user", dbUsername);
-			props.setProperty("password", dbPassword);
-			props.setProperty("initialSize", dbInitialConnections);
-			props.setProperty("maxActive", dbMaxConnections);
+			props.setProperty("user", DB_USERNAME);
+			props.setProperty("password", DB_PASSWORD);
+			props.setProperty("initialSize", DB_INIT_CONNECTIONS);
+			props.setProperty("maxActive", DB_MAX_CONNECTIONS);
 
 			ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
-					connUri, props);
+					URI, props);
 			PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(
 					connectionFactory, null);
 			ObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(
 					poolableConnectionFactory);
-			PoolingDataSource<PoolableConnection> datasource = new PoolingDataSource<>(
-					connectionPool);
+			dataSource = new PoolingDataSource<>(connectionPool);
 			Class.forName("org.apache.commons.dbcp2.PoolingDriver");
 			dbDriver = (PoolingDriver) DriverManager
 					.getDriver("jdbc:apache:commons:dbcp:");
-			dbDriver.registerPool(dbName, connectionPool);
-
-			return datasource;
-
+			dbDriver.registerPool(DB_NAME, connectionPool);
 		} catch (Exception ex) {
-			lgr.log(Level.SEVERE,
-					"Got error initializing data source: " + ex.getMessage(),
-					ex);
-			return null;
+			LOGGER.log(Level.SEVERE, "Got error initializing data source: "
+					+ ex.getMessage(), ex);
 		}
 	}
 }
