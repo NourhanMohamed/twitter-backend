28789a0b21f1f12e4fc93e87be5d77b079518e9f
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-04-16 01:25:22 +0200

    updated commands map

--- src/twitter/database/CommandsMap.java
+++ src/twitter/database/CommandsMap.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import twitter.database.commands.tweet.DeleteTweetCommand;
 import twitter.database.commands.tweet.FavoriteCommand;
+import twitter.database.commands.tweet.GetTweetCommand;
 import twitter.database.commands.tweet.NewTweetCommand;
 import twitter.database.commands.tweet.ReportTweetCommand;
 import twitter.database.commands.tweet.RetweetCommand;
@@ -14,6 +15,10 @@ import twitter.database.commands.user.ConfirmFollowCommand;
 import twitter.database.commands.user.FollowCommand;
 import twitter.database.commands.user.FollowersCommand;
 import twitter.database.commands.user.FollowingCommand;
+import twitter.database.commands.user.GetFavoritesCommand;
+import twitter.database.commands.user.GetMentionsCommand;
+import twitter.database.commands.user.GetTimelineCommand;
+import twitter.database.commands.user.GetUserCommand;
 import twitter.database.commands.user.GetUsersCommand;
 import twitter.database.commands.user.RegisterCommand;
 import twitter.database.commands.user.ReportUserCommand;
@@ -43,11 +48,11 @@ public class CommandsMap {
 		cmdMap.put("following", FollowingCommand.class);
 		cmdMap.put("unconfirmed_followers", UnconfirmedFollowersCommand.class);
 		cmdMap.put("update_user", UpdateUserCommand.class);
-		
-//		cmdMap.put("get_tweet", );
-//		cmdMap.put("get_retweeters");
-//		cmdMap.put("get_favoriters");
-//		cmdMap.put("");
+		cmdMap.put("get_user", GetUserCommand.class);
+		cmdMap.put("get_mentions", GetMentionsCommand.class);
+		cmdMap.put("get_tweet", GetTweetCommand.class);
+		cmdMap.put("timeline", GetTimelineCommand.class);
+		cmdMap.put("get_favorites", GetFavoritesCommand.class);
 	}
 	
 	public static Class<?> queryClass(String cmd){
