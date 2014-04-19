0eeaea407793e6ab8beadae331f778c391aedb24
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-04-19 16:09:50 +0200

    tweet class

---
+++ src/twitter/database/Tweet.java
@@ -0,0 +1,36 @@
+package twitter.database;
+
+import java.sql.Timestamp;
+
+@SuppressWarnings("unused")
+public class Tweet {
+	private Integer id;
+	private String tweet_text;
+	private String image_url;
+	private String created_at;
+	private User creator;
+	private User retweeter;
+	private User favoriter;
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setTweetText(String tweet_text) {
+		this.tweet_text = tweet_text;
+	}
+	public void setImageUrl(String image_url) {
+		this.image_url = image_url;
+	}
+	public void setCreatedAt(Timestamp timestamp) {
+		this.created_at = timestamp.toString();
+	}
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+	public void setRetweeter(User retweeter) {
+		this.retweeter = retweeter;
+	}
+	public void setFavoriter(User favoriter) {
+		this.favoriter = favoriter;
+	}
+}
