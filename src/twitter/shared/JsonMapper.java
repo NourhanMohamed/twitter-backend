d57da86123069cca0a593f0de29ba0b9db5fb790
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-04-21 14:10:48 +0200

    updated user and tweet main apps

--- src/twitter/shared/JsonMapper.java
+++ src/twitter/shared/JsonMapper.java
@@ -2,9 +2,6 @@ package twitter.shared;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -12,26 +9,17 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 
 public class JsonMapper {
-	private Logger lgr = Logger.getLogger(JsonMapper.class.getName());
 	private String json;
 
 	public JsonMapper(String json) {
 		this.json = json;
 	}
 
-	public Map<String, String> deserialize() {
+	public HashMap<String, String> deserialize() throws JsonParseException,
+			JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
-		try {
-			return mapper.readValue(json,
-					new TypeReference<HashMap<String, String>>() {
-			});
-		} catch (JsonParseException e) {
-			lgr.log(Level.SEVERE, e.getMessage(), e);
-		} catch (JsonMappingException e) {
-			lgr.log(Level.SEVERE, e.getMessage(), e);
-		} catch (IOException e) {
-			lgr.log(Level.SEVERE, e.getMessage(), e);
-		}
-		return null;
+		return mapper.readValue(json,
+				new TypeReference<HashMap<String, String>>() {
+				});
 	}
 }
--- src/twitter/shared/UserMain.java
+++ src/twitter/shared/UserMain.java
@@ -1,29 +1,86 @@
 package twitter.shared;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.TextMessage;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 
 import twitter.activemq.ActiveMQConfig;
 import twitter.activemq.Consumer;
 import twitter.concurrent.WorkerPool;
+import twitter.database.Command;
+import twitter.database.CommandsMap;
+import twitter.database.PostgresConnection;
 
 public class UserMain {
-	private static Logger lgr = Logger.getLogger(UserMain.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(UserMain.class
+			.getName());
 	private static WorkerPool pool = new WorkerPool(10);
-	
+	private static boolean run = true;
+
 	public static void main(String[] args) {
+		PostgresConnection.initSource();
+		CommandsMap.instantiate();
 		try {
-			new Consumer(new ActiveMQConfig("USERIN.QUEUE"));
+			Consumer c = new Consumer(new ActiveMQConfig("USER.INQUEUE"));
+			MessageConsumer consumer = c.connect();
+
+			while (run) {
+				Message msg = consumer.receive();
+				if (msg instanceof TextMessage) {
+					String msgTxt = ((TextMessage) msg).getText();
+					handleMsg(msgTxt);
+				}
+			}
+
+			c.disconnect();
 		} catch (JMSException e) {
-			lgr.log(Level.SEVERE, e.getMessage(), e);
+			LOGGER.log(Level.SEVERE, e.getMessage(), e);
 		}
 	}
-	
-	public static void handleMap(Map<String, String> map) {
-		pool.execute(new WorkerThread(map));
+
+	public static void shutdown() {
+		run = false;
 	}
-	
+
+	private static void handleMsg(String msg) {
+		JsonMapper json = new JsonMapper(msg);
+		HashMap<String, String> map = null;
+		try {
+			map = json.deserialize();
+		} catch (JsonParseException e1) {
+			LOGGER.log(Level.SEVERE, e1.getMessage(), e1);
+		} catch (JsonMappingException e1) {
+			LOGGER.log(Level.SEVERE, e1.getMessage(), e1);
+		} catch (IOException e1) {
+			LOGGER.log(Level.SEVERE, e1.getMessage(), e1);
+		}
+		if (map != null) {
+			map.put("app", "user");
+			if (map.containsKey("method")) {
+				Class<?> cmdClass = CommandsMap.queryClass(map.get("method"));
+				if (cmdClass == null) {
+					LOGGER.log(Level.SEVERE,
+							"Invalid Request. Class \"" + map.get("method")
+									+ "\" Not Found");
+				} else {
+					try {
+						Command c = (Command) cmdClass.newInstance();
+						c.setMap(map);
+						pool.execute((Runnable) c);
+					} catch (InstantiationException | IllegalAccessException e) {
+						LOGGER.log(Level.SEVERE, e.getMessage(), e);
+					}
+				}
+			}
+		}
+	}
 }
