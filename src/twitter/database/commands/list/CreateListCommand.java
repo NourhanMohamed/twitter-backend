dfe335474005b8488d566009609523061cbda9c7
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-04-27 00:28:47 +0200

    adjusted already existing list commands and tested them

--- src/twitter/database/commands/list/CreateListCommand.java
+++ src/twitter/database/commands/list/CreateListCommand.java
@@ -1,5 +1,6 @@
 package twitter.database.commands.list;
 
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -7,16 +8,22 @@ import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
 import org.postgresql.util.PSQLException;
 
 import twitter.database.Command;
+import twitter.database.CommandsHelp;
 import twitter.database.PostgresConnection;
+import twitter.shared.MyObjectMapper;
 
 public class CreateListCommand implements Command, Runnable {
 	private final Logger LOGGER = Logger.getLogger(CreateListCommand.class
 			.getName());
 	private HashMap<String, String> map;
-	
+
 	@Override
 	public void setMap(HashMap<String, String> map) {
 		this.map = map;
@@ -35,31 +42,49 @@ public class CreateListCommand implements Command, Runnable {
 			proc.setString(1, map.get("name"));
 			proc.setString(2, map.get("description"));
 			proc.setInt(3, Integer.parseInt(map.get("creator_id")));
-			proc.setBoolean(4, Boolean.getBoolean(map.get("private")));
+			proc.setBoolean(4, Boolean.parseBoolean(map.get("private")));
 			proc.execute();
 
+			MyObjectMapper mapper = new MyObjectMapper();
+			JsonNodeFactory nf = JsonNodeFactory.instance;
+			ObjectNode root = nf.objectNode();
+			root.put("app", map.get("app"));
+			root.put("method", map.get("method"));
+			root.put("status", "ok");
+			root.put("code", "200");
+			try {
+				CommandsHelp.submit(map.get("app"),
+						mapper.writeValueAsString(root), LOGGER);
+			} catch (JsonGenerationException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			} catch (JsonMappingException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			} catch (IOException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			}
 		} catch (PSQLException e) {
 			if (e.getMessage().contains("unique constraint")) {
-				if (e.getMessage().contains("(name)"))
-					System.out.println("list name already exists");
+				if (e.getMessage().contains("(name)")) {
+					CommandsHelp.handleError(map.get("app"), map.get("method"),
+							"List name already exists", LOGGER);
+				}
 			}
 			if (e.getMessage().contains("value too long")) {
-				if (e.getMessage().contains("(name)"))
-					System.out.println("list name exceeds 50 characters");
-				if (e.getMessage().contains("(description)"))
-					System.out.println("description exceeds 140 characters");
+				CommandsHelp.handleError(map.get("app"), map.get("method"),
+						"Too long input", LOGGER);
 			}
+			CommandsHelp.handleError(map.get("app"), map.get("method"),
+					"List name already exists", LOGGER);
 			LOGGER.log(Level.SEVERE, e.getMessage(), e);
 		} catch (SQLException e) {
+			CommandsHelp.handleError(map.get("app"), map.get("method"),
+					"List name already exists", LOGGER);
 			LOGGER.log(Level.SEVERE, e.getMessage(), e);
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		execute();
 	}
-
-	
-
 }
--- src/twitter/database/commands/list/UpdateListCommand.java
+++ src/twitter/database/commands/list/UpdateListCommand.java
@@ -1,22 +1,33 @@
 package twitter.database.commands.list;
 
+import java.io.IOException;
+import java.sql.Array;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
 import org.postgresql.util.PSQLException;
 
 import twitter.database.Command;
+import twitter.database.CommandsHelp;
 import twitter.database.PostgresConnection;
+import twitter.shared.MyObjectMapper;
 
 public class UpdateListCommand implements Command, Runnable {
 	private final Logger LOGGER = Logger.getLogger(UpdateListCommand.class
 			.getName());
 	private HashMap<String, String> map;
-	
+
 	@Override
 	public void setMap(HashMap<String, String> map) {
 		this.map = map;
@@ -24,32 +35,74 @@ public class UpdateListCommand implements Command, Runnable {
 
 	@Override
 	public void execute() {
+		String app = map.get("app");
+		String method = map.get("method");
 		try {
 			Connection dbConn = PostgresConnection.getDataSource()
 					.getConnection();
 			dbConn.setAutoCommit(true);
 			CallableStatement proc = dbConn
-					.prepareCall("{call update_list(?,?,?,?)}");
+					.prepareCall("{call update_list(?,?)}");
 			proc.setPoolable(true);
 
-			proc.setInt(1, Integer.parseInt(map.get("user_id")));
-			proc.setString(2, map.get("name"));
-			proc.setString(3, map.get("description"));
-			proc.setBoolean(4, Boolean.getBoolean(map.get("private")));		
+			proc.setInt(1, Integer.parseInt(map.get("list_id")));
+			map.remove("list_id");
+			map.remove("app");
+			map.remove("method");
+			Set<Entry<String, String>> set = map.entrySet();
+			Iterator<Entry<String, String>> iterator = set.iterator();
+			String[][] arraySet = new String[set.size()][2];
+			int i = 0;
+
+			while (iterator.hasNext()) {
+				Entry<String, String> entry = iterator.next();
+				String[] temp = { entry.getKey(), entry.getValue() };
+				arraySet[i] = temp;
+				i++;
+			}
+			Array array = dbConn.createArrayOf("text", arraySet);
+			proc.setArray(2, array);
 			proc.execute();
 
+			MyObjectMapper mapper = new MyObjectMapper();
+			JsonNodeFactory nf = JsonNodeFactory.instance;
+			ObjectNode root = nf.objectNode();
+			root.put("app", app);
+			root.put("method", method);
+			root.put("status", "ok");
+			root.put("code", "200");
+			try {
+				CommandsHelp.submit(app, mapper.writeValueAsString(root),
+						LOGGER);
+			} catch (JsonGenerationException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			} catch (JsonMappingException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			} catch (IOException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+			}
+
 		} catch (PSQLException e) {
+			if (e.getMessage().contains("unique constraint")) {
+				if (e.getMessage().contains("(name)")) {
+					CommandsHelp.handleError(app, method,
+							"List name already exists", LOGGER);
+				}
+			}
+			if (e.getMessage().contains("value too long")) {
+				CommandsHelp.handleError(app, method, "Too long input", LOGGER);
+			}
+			CommandsHelp.handleError(app, method, "List name already exists",
+					LOGGER);
 			LOGGER.log(Level.SEVERE, e.getMessage(), e);
 		} catch (SQLException e) {
+			CommandsHelp.handleError(app, method, e.getMessage(), LOGGER);
 			LOGGER.log(Level.SEVERE, e.getMessage(), e);
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		execute();
 	}
-
-	
-
 }
