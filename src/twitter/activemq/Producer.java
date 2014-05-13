4cc2518348a3d77d7c611b209aa8e13756e3138d
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-05-13 15:44:56 +0200

    attached the JMSCorrelationID to response message

--- src/twitter/activemq/Producer.java
+++ src/twitter/activemq/Producer.java
@@ -3,6 +3,7 @@ package twitter.activemq;
 import javax.jms.Connection;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 
@@ -13,12 +14,14 @@ public class Producer {
 		this.config = config;
 	}
 
-	public void send(String msg) throws JMSException {
+	public void send(String msg, String correlationID) throws JMSException {
 		Connection conn = config.connect();
 		Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		Destination destination = session.createQueue(config.getQueueName());
 		MessageProducer producer = session.createProducer(destination);
-		producer.send(session.createTextMessage(msg));
+		Message message = session.createTextMessage(msg);
+		message.setJMSCorrelationID(correlationID);
+		producer.send(message);
 		config.disconnect(conn);
 	}
 }
