922d52a55e3fe9fca7c0a46fe2b0c970aba42924
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2014-05-19 12:21:05 +0200

    Added some config to dbcp pool and added a resource release method

--- src/twitter/database/PostgresConnection.java
+++ src/twitter/database/PostgresConnection.java
@@ -2,6 +2,8 @@ package twitter.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -15,6 +17,8 @@ import org.apache.commons.dbcp2.PoolingDataSource;
 import org.apache.commons.dbcp2.PoolingDriver;
 import org.apache.commons.pool2.ObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
 
 public class PostgresConnection {
 	private static final Logger LOGGER = Logger
@@ -30,12 +34,7 @@ public class PostgresConnection {
 	private static PoolingDataSource<PoolableConnection> dataSource;
 
 	public static void shutdownDriver() throws SQLException {
-		shutdownConnection();
 		dbDriver.closePool(DB_NAME);
-	}
-
-	private static void shutdownConnection() throws SQLException {
-		dataSource.getConnection().close();
 	}
 
 	public static void printDriverStats() throws SQLException {
@@ -50,6 +49,34 @@ public class PostgresConnection {
 
 	public static PoolingDataSource<PoolableConnection> getDataSource() {
 		return dataSource;
+	}
+
+	public static void disconnect(ResultSet rs, PreparedStatement statment,
+			Connection conn) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+			}
+		}
+		if (statment != null) {
+			try {
+				statment.close();
+			} catch (SQLException e) {
+			}
+		}
+		if (conn != null) {
+			try {
+				System.out.println(conn.toString());
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+		try {
+			printDriverStats();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void initSource() {
@@ -71,13 +98,21 @@ public class PostgresConnection {
 					URI, props);
 			PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(
 					connectionFactory, null);
+			poolableConnectionFactory.setPoolStatements(true);
+
+			GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+			poolConfig.setMaxIdle(Integer.parseInt(DB_MAX_CONNECTIONS));
+			poolConfig.setMaxTotal(Integer.parseInt(DB_MAX_CONNECTIONS));
 			ObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(
-					poolableConnectionFactory);
-			dataSource = new PoolingDataSource<>(connectionPool);
+					poolableConnectionFactory, poolConfig);
+			poolableConnectionFactory.setPool(connectionPool);
+			
 			Class.forName("org.apache.commons.dbcp2.PoolingDriver");
 			dbDriver = (PoolingDriver) DriverManager
 					.getDriver("jdbc:apache:commons:dbcp:");
 			dbDriver.registerPool(DB_NAME, connectionPool);
+
+			dataSource = new PoolingDataSource<>(connectionPool);
 		} catch (Exception ex) {
 			LOGGER.log(Level.SEVERE, "Got error initializing data source: "
 					+ ex.getMessage(), ex);
